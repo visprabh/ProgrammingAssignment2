99008720c720850d83fcf6e80d0f6c3ddc781007
visprabh <vishnu.ait@gmail.com>
2014-08-20 15:39:13 -0700

    Added changes to handle caching scenarios if the data matrix is changed through set function

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,34 @@
-## Version:: v1.0
+## Version:: v1.1
 ## Author:: visprabh
 ## Description: 
 ##              Intent of the code is compute Inverse of a matrix and cache it. 
 ##              Make use of the cache if the same matrix is passed again for inverse computation
 ##              Assumption: Only square invertible matrix is passed as input parameter
 
+##              v1.1 change: added logic to handle caching scenario if matrix is changed through the function
 
 ## makeCacheMatrix(x):: 
 ##      This function creates a special "matrix" object that can cache it's inverse.
 ##      It takes square invertible matrix as input parameter
 
 makeCacheMatrix <- function(x=matrix()) {
-        inverseFlag <- FALSE              ## fLag used to check if the inverse matrix is already cached
+        inverseFlag <<- FALSE             ## fLag used to check if the inverse matrix is already cached
         inverseMatrix <- matrix()         ## initiallizing empty inverse matrix, it will be cached to store inverse of the original matrix
-        originalMatrix <- x               ## original matrix which will be cached to check if the matrix changed or not
+        originalMatrix <<- x              ## original matrix which will used to check if the matrix changed or not
         
         ## set function to set initial data and cache original matrix
         set <- function(y) {
-                x <<-y
-                inverseFlag<<-FALSE
-                originalMatrix <<-y
+                x <<-y     
         }
         
         ## function to get matrix which needs to be inversed
         getMatrix <- function() x
         
         ##function to set and cache inverse matrix, also set the inverse flag to TRUE
-        setInverse <- function(invOfx) {
+        setInverse <- function(invOfx, data) {
                 inverseMatrix<<-invOfx
                 inverseFlag<<-TRUE
+                originalMatrix <<- data   ## v1.1 change to handle cases if data matrix is changed through set function
         }
         
         ## Function to return inverse of the matrix
@@ -66,7 +66,7 @@ cacheSolve <- function(x) {
         inverseMatrix <- solve(data)
         
         ## function call to set and the inverse matrix for future use
-        x$setInverse(inverseMatrix)
+        x$setInverse(inverseMatrix, data)
         
         ##Return inverse matrix 
         inverseMatrix
